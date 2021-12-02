b85943b9f7ea0c13442ff6f2db58682e857249a2
liweixin <45406700@qq.com>
2021-12-02 18:40:45 +0800

    commit update

--- myapp/views.py
+++ myapp/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 
 
 # Create your views here.
+from myapp.models import Event
 
 
 def index(request):
@@ -23,9 +24,7 @@ def login_action(request):
             request.session['user'] = username  # 将session信息记录到浏览器
             respone = HttpResponseRedirect('/event_manage/')
             # respone.set_cookie('user', username, 3600)  #  添加浏览器cookie
-
             return respone
-
         else:
             return render(request, 'index.html', {'error': 'username or password error!'})
 
@@ -33,6 +32,7 @@ def login_action(request):
 # 发布会管理
 @login_required
 def event_manage(request):
+    event_list = Event.objects.all()
     # username = request.COOKIES.get('user', '')  # 读取浏览器cookie
     username = request.session.get('user', '')  # 读取浏览器的session
-    return render(request, "event_manage.html", {"user": username})
+    return render(request, "event_manage.html", {"user": username, "events": event_list})
